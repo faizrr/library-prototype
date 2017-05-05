032e0f152e7646e5ad5dc8abdabbe94b7849fd75
Roman <zummo3@gmail.com>
2017-05-06 01:00:57 +0300

    Persist auth state in localstorage

--- app/src/renderer/vuex/store.js
+++ app/src/renderer/vuex/store.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import createPersistedState from 'vuex-persistedstate'
 
 import * as actions from './actions'
 import * as getters from './getters'
@@ -11,5 +12,8 @@ export default new Vuex.Store({
   actions,
   getters,
   modules,
+  plugins: [
+    createPersistedState({ paths: ['auth'] })
+  ],
   strict: process.env.NODE_ENV !== 'production'
 })
